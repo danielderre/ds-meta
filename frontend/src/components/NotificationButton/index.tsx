a2d19def7d33e55fec0bd5304f348b9d8888a04f
danielderre <danielderre@outlook.com>
2022-07-17 20:38:48 -0300

    [new]: Axios, useEffect first request

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,12 @@
-import icon from '../../assets/img/notification-button.svg';
-import './styles.css';
+import icon from "../../assets/img/notification-button.svg";
+import "./styles.css";
 
 function NotificationButton() {
-  return (
-    <div className="dsmeta-red-btn">
-      <img src={icon} alt="Notification" />
-    </div>
-  )
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notification" />
+        </div>
+    );
 }
 
 export default NotificationButton;
